8c239b10dc0460118dba95c3ef647d7c28b4e80a
AlexParashkevov <64553049+AlexParashkevov@users.noreply.github.com>
2021-01-26 20:50:38 +0200

    Fixed Bugs

--- script.js
+++ script.js
@@ -13,13 +13,24 @@ const bntRoll = document.querySelector('.btn--roll');
 const bntHold = document.querySelector('.btn--hold');
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  Player0El.classList.remove('player--winner');
+  Player1El.classList.remove('player--winner');
+  Player0El.classList.add('player--active');
+  Player1El.classList.remove('player--active');
+};
+init();
 const switchplayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   //switch to next player
@@ -70,22 +81,4 @@ bntHold.addEventListener('click', function () {
     }
   }
 });
-bntNew.addEventListener('click', function () {
-  playing = true;
-  currentScore = 0;
-  activePlayer = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  document.getElementById(`score--0`).textContent = 0;
-  document.getElementById(`score--1`).textContent = 0;
-  diceEl.classList.toggle('hidden');
-  Player0El.classList.remove('player--winner');
-  Player1El.classList.remove('player--winner');
-  Player0El.classList.add('player--active');
-  Player1El.classList.remove('player--active');
-  document.getElementById(`current--0`).textContent = 0;
-  document.getElementById(`current--1`).textContent = 0;
-});
+bntNew.addEventListener('click', init);
